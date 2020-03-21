2757381342d6688f4178aa3686dec8161f45ddef
manasbekoff <manasbekov@gmail.com>
2020-03-21 18:06:13 +0600

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 1; i < 5; i++) {
-            Runner runner = new Runner("Runner " + i);
-            runner.setRunnerNumber(i + 1);
-            runner.start();
-            runner.join();
-        }
-        for (int i = 5; i > 1 ; i--) {
-            Runner runner1 = new Runner("Runner " + i);
-            runner1.setRunnerNumber(i-1);
-            runner1.start();
-            runner1.join();
+        try {
+            for (int i = 1; i < 5; i++) {
+                Runner runner = new Runner("Runner " + i);
+                runner.setRunnerNumber(1+i);
+                runner.setPriority(i);
+                runner.start();
+                runner.join();
+            }
+            for (int i = 5; i > 1; i--) {
+                Runner runner1 = new Runner("Runner " + i);
+                runner1.setRunnerNumber(i - 1);
+                runner1.setPriority(i);
+                runner1.start();
+                runner1.join();
+            }
+        } catch (Exception e) {
         }
     }
 }
--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -16,16 +16,13 @@ public class Runner extends Thread {
     private int runnerNumber;
 
     @Override
-    public void run() {
+    public synchronized void run() {
         try {
-            if (getRunnerNumber() == 4)
-                System.out.println(this.getName() +" Бежит к финишу");
-            if ( getRunnerNumber() == 0 ) {
-                System.out.println(this.getName() + " Берет палочку ");
+            if (getPriority() == 5) {
+                System.out.println(this.getName() + " Бежит к финишу");
             } else
                 System.out.println(this.getName() + " Берет палочку");
             System.out.println(this.getName() + " бежит к Runner " + getRunnerNumber());
-
             if (getRunnerNumber() == 1)
                 System.out.println("Runner 1 берет палочку");
             sleep(500);
